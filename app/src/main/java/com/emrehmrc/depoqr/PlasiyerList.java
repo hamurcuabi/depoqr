36a6dc6f5e4616e575e3f3b93a9efcae44334e09
cenah <cen.ahmedar@gmail.com>
2018-03-28 13:43:44 +0300

    Plasiyer

--- app/src/main/java/com/emrehmrc/depoqr/PlasiyerList.java
+++ app/src/main/java/com/emrehmrc/depoqr/PlasiyerList.java
@@ -1,13 +1,26 @@
 package com.emrehmrc.depoqr;
 
+import android.annotation.TargetApi;
+import android.app.ActivityOptions;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import static com.emrehmrc.depoqr.AnaSayfa.MyPREFERENCES;
 
@@ -18,10 +31,16 @@ public class PlasiyerList extends AppCompatActivity {
     Bundle bundle;
     Vibrator vibrator;
     String memberid,comid;
+    EditText cariArama;
+    ListView lst_Cari;
+    TextView tx_toplam;
+    Button btn_satisbasla;
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_grupbarkod);
+        setContentView(R.layout.activity_plasiyerlist);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         sharedPreferences = getApplicationContext().getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
@@ -31,5 +50,59 @@ public class PlasiyerList extends AppCompatActivity {
         ab = getSupportActionBar();
         ab.setTitle("Plasiyer Satiş");
         ab.setBackgroundDrawable(getResources().getDrawable(R.drawable.arkaplan));
+        progressBar = (ProgressBar) findViewById(R.id.pbbarP);
+
+        btn_satisbasla = (Button) findViewById(R.id.btn_satisbasla);
+        tx_toplam = (TextView) findViewById(R.id.tx_toplam);
+        cariArama = (EditText) findViewById(R.id.CariArama);
+
+        FillList fillList = new FillList();
+        fillList.execute("");
+
+
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.plasiyersatis, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.anasayfa) {
+
+            Intent i = new Intent(PlasiyerList.this, SliderMenu.class);
+            bundle = ActivityOptions.makeSceneTransitionAnimation(PlasiyerList.this).toBundle();
+            finish();
+        } else if (id == R.id.geri) {
+            finish();
+            onBackPressed();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    public class FillList extends AsyncTask<String,String,String>{
+
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            progressBar.setVisibility(View.GONE);
+        }
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
     }
 }
