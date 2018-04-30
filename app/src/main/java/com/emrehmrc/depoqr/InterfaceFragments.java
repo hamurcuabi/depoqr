233d1faa500ad30de6f5340d1fee4eb67098bed1
cenah <cen.ahmedar@gmail.com>
2018-04-30 10:42:06 +0300

    bilgiEkrani

--- app/src/main/java/com/emrehmrc/depoqr/InterfaceFragments.java
+++
@@ -1,9 +0,0 @@
-package com.emrehmrc.depoqr;
-
-/**
- * Created by Emre Hmrc on 20.02.2018.
- */
-
-public interface InterfaceFragments {
-    public void Degerler(String sevkno,String bacodeno);
-}
