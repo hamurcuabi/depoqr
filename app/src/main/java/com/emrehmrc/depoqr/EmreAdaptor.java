929a3fc8340bed353bd63fe385d893b755efa1e2
cenah <cen.ahmedar@gmail.com>
2018-05-17 11:28:26 +0300

    plasiyercalismasi

--- app/src/main/java/com/emrehmrc/depoqr/EmreAdaptor.java
+++ app/src/main/java/com/emrehmrc/depoqr/EmreAdaptor.java
@@ -1,9 +1,7 @@
 package com.emrehmrc.depoqr;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.graphics.Color;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -27,11 +25,10 @@ public class EmreAdaptor extends RecyclerView.Adapter<EmreAdaptor.MyViewHolder> 
 
     ArrayList<SevkiyatÜrünleriRecyclerView> datalist;
     LayoutInflater layoutInflater;
-Context context;
+
     public EmreAdaptor(Context context, ArrayList<SevkiyatÜrünleriRecyclerView> data) {
         layoutInflater = LayoutInflater.from(context);
         this.datalist = data;
-        this.context = context;
 
     }
 
@@ -55,127 +52,102 @@ Context context;
 
         final float deneme = datalist.get(position).getFirstamount();
         final float deneme2 = datalist.get(position).getSecondamount();
+        holder.edtfirstamount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+                try {
+                    if (deneme < Float.parseFloat(holder.edtfirstamount.getText().toString())) {
+                        holder.uyari1.setText("MİKTAR AŞILDI!");
+                        holder.checkBox.setChecked(false);
+                        holder.checkBox.setEnabled(false);
+                        datalist.get(position).setChecked(false);
+
+                    } else if (deneme >= Float.parseFloat(holder.edtfirstamount.getText().toString())) {
+                        holder.uyari1.setText("");
+                        holder.checkBox.setChecked(true);  //bunu
+                        holder.checkBox.setEnabled(true); //icindeki ture
+                        datalist.get(position).setFirstamount(Float.parseFloat(holder.edtfirstamount.getText().toString()));
+                        datalist.get(position).setChecked(true);
+
+                    }else {
+                        holder.uyari1.setText("");//bunu
+                        holder.checkBox.setEnabled(true); // bunu
+                        holder.checkBox.setChecked(true); //bunu
+                        datalist.get(position).setChecked(true);
+
+                    }
+
+                } catch (Exception ex) {
+                    holder.uyari1.setText("MİKTAR BOŞ GİRİLDİ!");
+                    holder.checkBox.setChecked(false);
+                    holder.checkBox.setEnabled(false);
+                    datalist.get(position).setChecked(false);
+                }
+
+            }
+        });
+        holder.edtsecondamount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                try {
+                    if (deneme2 < Float.parseFloat(holder.edtsecondamount.getText().toString())) {
+                        holder.uyari2.setText("MİKTAR AŞILDI!");
+                        holder.checkBox.setChecked(false);
+                        holder.checkBox.setEnabled(false);
+                        datalist.get(position).setChecked(false);
+
+                    } else if (deneme2 >= Float.parseFloat(holder.edtsecondamount.getText().toString())) {
+                        holder.uyari2.setText("");
+                        holder.checkBox.setChecked(true);  //bunu
+                        holder.checkBox.setEnabled(true); //icindeki ture
+                        datalist.get(position).setSecondamount(Float.parseFloat(holder.edtsecondamount.getText().toString()));
+                        datalist.get(position).setChecked(true);
+
+                    }else {
+                        holder.uyari1.setText("");//bunu
+                        holder.checkBox.setEnabled(true); // bunu
+                        holder.checkBox.setChecked(true); //bunu
+                        datalist.get(position).setChecked(true);
+
+                    }
+                } catch (Exception ex) {
+                    holder.uyari2.setText("MİKTAR BOŞ GİRİLDİ!");
+                    holder.checkBox.setChecked(false);
+                    holder.checkBox.setEnabled(false);
+                    datalist.get(position).setChecked(false);
+
+                }
+
+
+            }
+        });
         holder.checkBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 datalist.get(position).setChecked(!datalist.get(position).isChecked());
             }
         });
-        holder.lnr1.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                LayoutInflater inflater = LayoutInflater.from(context);
-                View subView = inflater.inflate(R.layout.adapterchanger, null);
-                final EditText Cfirstamount = (EditText)subView.findViewById(R.id.edtfirstamount);
-                final EditText Csecandamount = (EditText)subView.findViewById(R.id.edtsecondtamount);
-                final TextView CfirstamountB = (TextView) subView.findViewById(R.id.firstunitamount);
-                final TextView CsecondamuntB = (TextView) subView.findViewById(R.id.seconunitamount);
-                final TextView CfirstUnit = (TextView) subView.findViewById(R.id.firstunit);
-                final TextView CsecondUnit = (TextView) subView.findViewById(R.id.seconunit);
-                final TextView txtuyari1 = (TextView) subView.findViewById(R.id.txtuyari1);
-                final TextView txtuyari2 = (TextView) subView.findViewById(R.id.txtuyari2);
-
-                Cfirstamount.setText(holder.edtfirstamount.getText().toString());
-                Csecandamount.setText(holder.edtsecondamount.getText().toString());
-                CfirstamountB.setText(holder.firstamount.getText().toString());
-                CsecondamuntB.setText(holder.secondamount.getText().toString());
-                Cfirstamount.addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                    }
-
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                        try {
-                            if (deneme < Float.parseFloat(Cfirstamount.getText().toString())) {
-                                txtuyari1.setText("MİKTAR AŞILDI!");
-
-
-                            } else if (deneme >= Float.parseFloat(Cfirstamount.toString())) {
-                                txtuyari1.setText("");
-
-                                datalist.get(position).setFirstamount(Float.parseFloat(Cfirstamount.getText().toString()));
-
-
-                            }else {
-                                txtuyari1.setText("");//bunu
-
-                            }
-
-                        } catch (Exception ex) {
-                            txtuyari1.setText("MİKTAR BOŞ GİRİLDİ!");
-
-                        }
-                    }
-                });
-                Csecandamount.addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                    }
-
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                        try {
-                            if (deneme2 < Float.parseFloat(Csecandamount.getText().toString())) {
-                                txtuyari2.setText("MİKTAR AŞILDI!");
-
-
-                            } else if (deneme2 >= Float.parseFloat(Csecandamount.getText().toString())) {
-                                txtuyari2.setText("");
-
-                                datalist.get(position).setSecondamount(Float.parseFloat(Csecandamount.getText().toString()));
-
-
-                            }else {
-                                txtuyari2.setText("");//bunu
-
-
-                            }
-                        } catch (Exception ex) {
-                            txtuyari2.setText("MİKTAR BOŞ GİRİLDİ!");
-
-                        }
-
-
-                    }
-                });
-                android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(context);
-               // builder.setTitle("AÇIKLAMA");
-                //builder.setMessage("ZORUNLU DEĞIL");
-                builder.setView(subView);
-                android.app.AlertDialog alertDialog = builder.create();
-                builder.setPositiveButton("Kaydet", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-
-                    }
-                });
-
-                builder.setNegativeButton("Iptal", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                });
-
-                builder.show();
-
-                return true;
-            }
-        });
-
     }
 
     @Override
@@ -255,3 +227,8 @@ Context context;
         }
     }
 }
+
+
+
+
+
